685dc69509b5f9dcfe172db83d8045c7bb837d49
michi <a@a>
2025-06-09 00:15:17 +0200

    small improvement

--- gopamixer/gopamixer.go
+++ gopamixer/gopamixer.go
@@ -5,10 +5,56 @@ import (
 	"github.com/rivo/tview"
 )
 
+var focusIndex = 0
+var App *tview.Application
+
 func Somestuff() {
 	//fmt.Println("Hello, World!")
 
-	box := tview.NewBox().SetBorder(true).SetTitle(" GoPa mixer! ")
+	App = tview.NewApplication()
+
+	headerBox := tview.NewBox().SetBorder(true).SetTitle(" GoPa mixer! ")
+
+	vols := []*tview.Box{DrawVolumeBar("one"), DrawVolumeBar("two"), DrawVolumeBar("three")}
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(headerBox, 3, 1, false)
+
+	for _, vol := range vols {
+		flex.AddItem(vol, 7, 1, true)
+	}
+	flex.AddItem(tview.NewBox(), 0, 1, false) // fills the rest of the space
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyUp {
+			if focusIndex > 0 {
+				focusIndex--
+				App.SetFocus(vols[focusIndex])
+			}
+			return nil
+		} else if event.Key() == tcell.KeyDown {
+			if focusIndex < len(vols)-1 {
+				focusIndex++
+				App.SetFocus(vols[focusIndex])
+			}
+			return nil
+		} else if event.Key() == tcell.KeyRune {
+			if event.Rune() == 'q' {
+				App.Stop()
+				return nil
+			}
+		}
+		return event
+	})
+
+	if err := App.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func DrawVolumeBar(name string) *tview.Box {
+	box := tview.NewBox().SetBorder(true).SetTitle(" Some Volume: " + name + " ")
 	box.SetDrawFunc(func(screen tcell.Screen, x, y, width, height int) (int, int, int, int) {
 
 		ypos := y + 2
@@ -37,7 +83,9 @@ func Somestuff() {
 
 		return x + 1, ypos, width - 2, height - 5
 	})
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
-	}
+	/*box.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		panic("ohnoo")
+		//return event
+	})*/
+	return box
 }
